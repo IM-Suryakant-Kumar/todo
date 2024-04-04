07676ff770b2284556e40869129903ec84f70533
IM-Suryakant-Kumar <sujeetsaxena78@gmail.com>
2024-04-04 22:44:09 +0530

    feat: implemented redux persist

--- src/features/store.js
+++ src/features/store.js
@@ -1,8 +1,36 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
 import { taskReducer } from "./reducers";
+import storage from "redux-persist/lib/storage";
+import {
+	FLUSH,
+	PAUSE,
+	PERSIST,
+	PURGE,
+	REGISTER,
+	REHYDRATE,
+	persistReducer,
+} from "redux-persist";
+
+const persistConfig = {
+	key: "root",
+	version: 1,
+	storage,
+};
+
+const reducer = combineReducers({
+	task: taskReducer,
+});
+
+const persistedReducer = persistReducer(persistConfig, reducer);
 
 const store = configureStore({
-	reducer: { task: taskReducer },
+	reducer: persistedReducer,
+	middleware: getDefaultMiddleware =>
+		getDefaultMiddleware({
+			serializableCheck: {
+				ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+			},
+		}),
 });
 
 export default store;
--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,15 @@ import App from "./App.jsx";
 import "./index.css";
 import { Provider } from "react-redux";
 import store from "./features/store.js";
+import { PersistGate } from "redux-persist/integration/react";
+import { persistStore } from "redux-persist";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
 	<React.StrictMode>
 		<Provider store={store}>
-			<App />
+			<PersistGate persistor={persistStore(store)}>
+				<App />
+			</PersistGate>
 		</Provider>
 	</React.StrictMode>
 );
